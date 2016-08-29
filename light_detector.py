aece2ca12041d64c6c0a4a9de6702fe5e5159eb5
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-08-29 09:41:26 +0200

    Add circle around detected light

--- light_detector.py
+++ light_detector.py
@@ -13,9 +13,19 @@ frame = cv2.imread(sys.argv[1])
 gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 _, gray_frame = cv2.threshold(gray_frame, MIN_BRIGHTNESS_THRESHOLD, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(gray_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-lightContours = [contour for contour in contours if cv2.contourArea(contour) >= MIN_LIGHT_AREA]
-print "Found " + str(len(lightContours)) + " lights"
-cv2.drawContours(frame, lightContours, -1, (0, 255, 0))
-cv2.imshow("", frame)
+lights = []
+for contour in contours:
+  m = cv2.moments(contour)
+  area = m["m00"]
+  if (area >= MIN_LIGHT_AREA):
+    x = int(m["m10"] / m["m00"])
+    y = int(m["m01"] / m["m00"])
+    lights.append({"x": x, "y": y, "area": area, "contour": contour})
 
+print "Found " + str(len(lights)) + " lights"
+cv2.drawContours(frame, [light["contour"] for light in lights], -1, (0, 255, 0))
+for light in lights:
+  cv2.circle(frame, (light["x"], light["y"]), MIN_LIGHT_AREA, (0, 0, 255))
+
+cv2.imshow("", frame)
 cv2.waitKey()
