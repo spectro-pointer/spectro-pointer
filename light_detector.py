8192ea73f6929dd5f597f183eb0be29422603b3f
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-08-29 09:17:07 +0200

    Show # of lights and accept photo as cmdline arg

--- light_detector.py
+++ light_detector.py
@@ -1,13 +1,20 @@
 import cv2
+import sys
 
 MIN_BRIGHTNESS_THRESHOLD = 150
-MIN_LIGHT_AREA = 20
+MIN_LIGHT_AREA = 15
 
-frame = cv2.imread("photos/small4.jpg")
+if len(sys.argv) != 2:
+  print "Expected 1 argument: The photo to analyze"
+  quit(1)
+
+frame = cv2.imread(sys.argv[1])
+
 gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 _, gray_frame = cv2.threshold(gray_frame, MIN_BRIGHTNESS_THRESHOLD, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(gray_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 lightContours = [contour for contour in contours if cv2.contourArea(contour) >= MIN_LIGHT_AREA]
+print "Found " + str(len(lightContours)) + " lights"
 cv2.drawContours(frame, lightContours, -1, (0, 255, 0))
 cv2.imshow("", frame)
 
