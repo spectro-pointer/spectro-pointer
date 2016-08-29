2a785649ec550c23a70a841c0cf1317f7f21cf8c
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-08-29 08:50:14 +0200

    findContours returns a tuple with 2 values

--- light_detector.py
+++ light_detector.py
@@ -5,8 +5,8 @@ MIN_BRIGHTNESS_THRESHOLD = 150
 frame = cv2.imread("photos/small4.jpg")
 gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 _, gray_frame = cv2.threshold(gray_frame, MIN_BRIGHTNESS_THRESHOLD, 255, cv2.THRESH_BINARY)
-contours = cv2.findContours(gray_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-cv2.drawContours(frame, contours[0], -1, (0, 255, 0))
+contours, _ = cv2.findContours(gray_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+cv2.drawContours(frame, contours, -1, (0, 255, 0))
 cv2.imshow("", frame)
 
 cv2.waitKey()
