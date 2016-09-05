c584adc4162f8b4b71fb4889d088d775d94192b6
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-09-05 20:18:18 +0200

    Add homing procedure to elevation stepper

--- elevation_controller_server.py
+++ elevation_controller_server.py
@@ -5,43 +5,49 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 class ElevationController:
     DIRECTION_GPIO = 36
     PULSE_GPIO = 32
-    HOME1_GPIO = 24
-    HOME2_GPIO = 26
+    # There is ~150 degrees between the two home sensors
+    HOME_DOWN_GPIO = 24
+    HOME_UP_GPIO = 26
     MICROSTEPS = 400
     GEAR_RATIO = 48
-    HOMING_DIRECTION = True
+    DOWN_DIRECTION = True
+    UP_DIRECTION = not ElevationController.DOWN_DIRECTION
 
     def __init__(self):
-        GPIO.setup(self.HOME1_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        GPIO.setup(self.HOME2_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        GPIO.setup(self.HOME_DOWN_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        GPIO.setup(self.HOME_UP_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
         self.__stepper = Stepper(self.MICROSTEPS * self.GEAR_RATIO, self.DIRECTION_GPIO, self.PULSE_GPIO)
 
-        print '1st home: ' + str(self.is_home1())
-        print '2nd home: ' + str(self.is_home2())
-
-        raise ValueError("nooooo")
-
         steps = 0
-        while self.is_home():
-            self.__stepper.pulse(self.HOMING_DIRECTION)
+        while self.is_home_up():
+            self.__stepper.pulse(self.DOWN_DIRECTION)
             steps += 1
             if (steps > self.total_steps()):
                 raise ValueError("Cannot home the elevation stepper. It is likely not moving or the sensor is broken.")
-        while not self.is_home():
-            self.__stepper.pulse(self.HOMING_DIRECTION)
+        while not self.is_home_up():
+            self.__stepper.pulse(self.UP_DIRECTION)
             steps += 1
             if (steps > self.total_steps()):
                 raise ValueError("Cannot home the elevation stepper. It is likely not moving or the sensor is broken.")
         self.__stepper.reset_position()
 
+        steps = 0
+        self.amplitude = 0
+        while not self.is_home_down():
+            self.__stepper.pulse(self.DOWN_DIRECTION)
+            steps += 1
+            self.amplitude += 1
+            if (steps > self.total_steps()):
+                raise ValueError("Cannot home the elevation stepper. It is likely not moving or the sensor is broken.")
+
     def is_home(self):
-        return self.is_home1() or self.is_home2()
+        return self.is_home_down() or self.is_home_up()
 
-    def is_home1(self):
-        return not GPIO.input(self.HOME1_GPIO)
+    def is_home_down(self):
+        return not GPIO.input(self.HOME_DOWN_GPIO)
 
-    def is_home2(self):
-        return not GPIO.input(self.HOME2_GPIO)
+    def is_home_up(self):
+        return not GPIO.input(self.HOME_UP_GPIO)
 
     def position(self):
         return self.__stepper.position()
