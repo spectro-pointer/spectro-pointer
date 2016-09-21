63929b96119d0e7da8f8a83459f1301f21b29f44
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-09-21 11:07:02 +0200

    Busca: Restore elevation after tracking

--- busca.py
+++ busca.py
@@ -151,12 +151,11 @@ def scan():
         elevation_controller.move_to(elevation*(1.0 / ELEVATION_STEPS) + (1.0 / ELEVATION_STEPS) / 2.0)
         for azimuth in range(0, azimuth_controller.total_steps(), 480*2):
             azimuth_controller.move_left(480*2)
-            old_position = azimuth_controller.position()
-            print "Azimuth: " + str(azimuth_controller.position()) + " Elevation: " + str(elevation_controller.position())
+            old_azimuth = azimuth_controller.position()
+            old_elevation = elevation_controller.position()
             process()
-            new_position = azimuth_controller.position()
-            print "New position " + str(new_position) + ", old position " + str(old_position)
-            azimuth_controller.move_to(old_position)
+            azimuth_controller.move_to(old_azimuth)
+            elevation_controller.move_to(old_elevation)
 
 while True:
     scan()
