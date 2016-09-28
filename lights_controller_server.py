60fc4d7adad0fde2fe2b4190fb2f96d37b2ff457
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-09-28 17:51:36 +0200

    Forgot to start the threads

--- lights_controller_server.py
+++ lights_controller_server.py
@@ -59,5 +59,8 @@ if __name__ == '__main__':
     t1 = threading.Thread(target = track_lights, args = (controller, ))
     t2 = threading.Thread(target = serve_requests, args = (controller, ))
 
+    t1.start()
+    t2.start()
+
     t1.join()
     t2.join()
