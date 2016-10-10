031897404ee335da087c9e5520bdd637ce43d671
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-10-10 18:03:06 +0200

    Demo light controller now shows captured images

--- demo_light_controller.py
+++ demo_light_controller.py
@@ -1,10 +1,17 @@
-import xmlrpclib
-import time
+import rpyc
+from lights import *
+import numpy as np
 
-IP = "127.0.0.1"
-
-controller = xmlrpclib.ServerProxy("http://" + IP + ":8003")
+controller = rpyc.connect("127.0.0.1", 8003)
 
 print "Getting..."
-r = controller.get()
+r = controller.root.get()
 print "Done "  + str(len(r[0]))
+
+print "Length: " + str(len(r[1]))
+
+while True:
+    r = controller.root.get()
+    im = np.fromstring(r[1], dtype=np.uint8).reshape((480, 640, 3))
+    cv2.imshow("foo", im)
+    cv2.waitKey(100)
