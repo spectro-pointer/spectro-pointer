039484c759bc49989d01178bccb1f5319aa5f868
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-10-10 22:57:23 +0200

    Lights controller demo: Get tracker state

--- demo_light_controller.py
+++ demo_light_controller.py
@@ -4,14 +4,9 @@ import numpy as np
 
 controller = rpyc.connect("127.0.0.1", 8003)
 
-print "Getting..."
-r = controller.root.get()
-print "Done "  + str(len(r[0]))
-
-print "Length: " + str(len(r[1]))
-
 while True:
-    r = controller.root.get()
-    im = np.fromstring(r[1], dtype=np.uint8).reshape((480, 640, 3))
+    lights, tracker, im = controller.root.get()
+    print "Found " + str(len(lights)) + " lights, " + str(len(tracker)) + " of which are tracked"
+    im = np.fromstring(im, dtype = np.uint8).reshape((480, 640, 3))
     cv2.imshow("foo", im)
     cv2.waitKey(100)
