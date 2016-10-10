d3793d8b84267bc78c0ddb7e28ccc581ddc92c84
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-10-10 17:22:47 +0200

    Fix typo

--- lights_controller_server.py
+++ lights_controller_server.py
@@ -28,7 +28,7 @@ class LightsController():
 
     def get(self):
         self.lock.acquire()
-        result = (copy.deepcopy(self.lights), im.tostring())
+        result = (copy.deepcopy(self.lights), self.im.tostring())
         self.lock.release()
 
         return result
