a145e394dad3a71a5eb258609ec7fb9dd77668c7
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-10-10 23:00:26 +0200

    Light tracker: Reduce max displacement and resize factor

--- lights.py
+++ lights.py
@@ -28,8 +28,8 @@ class LightDetector:
         return lights
 
 class LightTracker:
-    MAX_DISPLACEMENT = 25
-    MAX_RESIZING_FACTOR = 2.0
+    MAX_DISPLACEMENT = 4
+    MAX_RESIZING_FACTOR = 1.5
     DISPLACEMENT_WEIGHT = 0.8
     RESIZING_FACTOR_WEIGHT = 1.0 - DISPLACEMENT_WEIGHT
 
