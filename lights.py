774457d22ae1223ae274b698cd29cb2a6ea3db6b
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2016-10-11 22:32:45 +0200

    Light tracker: Increase max displacement to 10px

--- lights.py
+++ lights.py
@@ -29,7 +29,7 @@ class LightDetector:
         return lights
 
 class LightTracker:
-    MAX_DISPLACEMENT = 6
+    MAX_DISPLACEMENT = 10
     MAX_RESIZING_FACTOR = 1.5
     DISPLACEMENT_WEIGHT = 0.8
     RESIZING_FACTOR_WEIGHT = 1.0 - DISPLACEMENT_WEIGHT
