d97ed410fb2fbe81b9d8311351529dc2fe5adafd
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2017-07-28 15:21:35 +0200

    Fix the light detection to also work with daylight

--- lights.py
+++ lights.py
@@ -9,22 +9,26 @@ class Light:
         self.last_seen = 0
 
 class LightDetector:
-    MIN_BRIGHTNESS_THRESHOLD = 130
-    MIN_LIGHT_AREA = 3
+    MIN_BRIGHTNESS_THRESHOLD = 87
+    MIN_LIGHT_AREA = 2
 
     def detect(self, im):
         gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     
         _, gray_im = cv2.threshold(gray_im, self.MIN_BRIGHTNESS_THRESHOLD, 255, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(gray_im.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, _ = cv2.findContours(gray_im.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
         lights = []
-
         for contour in contours:
-            m = cv2.moments(contour)
-            area = m["m00"]
+            area = len(contour)
             if area >= self.MIN_LIGHT_AREA:
-                x = int(m["m10"] / m["m00"])
-                y = int(m["m01"] / m["m00"])
+                x = []
+                y = []
+                for point in contour:
+                    x.append(point[0][0])
+                    y.append(point[0][1])
+                x = sum(x) / len(x)
+                y = sum(y) / len(y)
+
                 lights.append(Light(x, y, area))
 
         return lights
