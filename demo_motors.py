c3961c67596e04e201bcf7cd19f0613713118a46
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2017-08-04 21:00:29 +0200

    Demo motores is now interractive

--- demo_motors.py
+++ demo_motors.py
@@ -1,18 +1,62 @@
 import xmlrpclib
 import time
 
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the
+screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
 MOTORES_IP = "127.0.0.1"
 
 elevation_controller = xmlrpclib.ServerProxy("http://" + MOTORES_IP + ":8001")
 azimuth_controller = xmlrpclib.ServerProxy("http://" + MOTORES_IP + ":8000")
 
-print "Moving to the blue LED.."
+getch = _Getch()
+while True:
+  c = getch()
+  if c == 'w':
+    p = elevation_controller.position() - 0.003
+    elevation_controller.move_to(p)
+  elif c == 's':
+    p = elevation_controller.position() + 0.003
+    elevation_controller.move_to(p)
+  elif c == 'a':
+    azimuth_controller.move_left(5)
+  elif c == 'd':
+    azimuth_controller.move_right(5)
+  else:
+    print 'Unknown command!'
+    exit()
 
-elevation_controller.move_to(0.678)
-azimuth_controller.move_to(17465)
-#azimuth_controller.move_to(17370)
-#azimuth_controller.move_to(17290)
-
-print "Done, current position:"
-print "  Elevation: %s" % (elevation_controller.position())
-print "  Azimuth: %s" % (azimuth_controller.position())
+  print "New positon: %f %d" % (elevation_controller.position(), azimuth_controller.position())
