50d78b5c8f602f1a591fb41d089b6cf9f179288f
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2017-08-08 02:09:26 +0200

    Coli demo: Show optical fiber location

--- demo_coli_controller.py
+++ demo_coli_controller.py
@@ -7,5 +7,6 @@ controller = xmlrpclib.ServerProxy("http://127.0.0.1:8002")
 while True:
     im_str = str(controller.get_image())
     im = np.fromstring(im_str, dtype = np.uint8).reshape((170, 190, 3))
+    cv2.circle(im, (75, 77), 5, (0, 0, 255), 1)
     cv2.imshow("coli", im)
     cv2.waitKey(333)
