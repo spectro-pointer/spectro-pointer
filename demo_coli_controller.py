4ff1e0f45653c180f845fc1d0fbe2ad6a4056000
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2017-09-13 14:46:21 +0200

    Demo coli: Adjust the center of the fiber for after the flip

--- demo_coli_controller.py
+++ demo_coli_controller.py
@@ -7,6 +7,6 @@ controller = xmlrpclib.ServerProxy("http://127.0.0.1:8002")
 while True:
     im_str = str(controller.get_image())
     im = np.fromstring(im_str, dtype = np.uint8).reshape((387-100, 443-185, 3))
-    cv2.circle(im, (128, 150), 5, (0, 0, 255), 1)
+    cv2.circle(im, (128, 137), 5, (0, 0, 255), 1)
     cv2.imshow("coli", im)
     cv2.waitKey(333)
